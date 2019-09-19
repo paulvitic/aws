c51bfdb57b8886fa5e79376c0e36213beb6308ed
pvitic <paul.vitic@smava.de>
2019-09-19 12:01:21 +0200

    changes on namings of environment and stacks

--- ecs-without-load-balancer/environment.sh
+++ ecs-without-load-balancer/environment.sh
@@ -1,28 +1,39 @@
 #!/usr/bin/env bash
 
+ENVIRONMENT_NAME_SUFFIX=-env
 DIR=$(pwd)
-ENV_ADMIN_STORAGE_TEMPLATE=$DIR/1-environment-admin-storage.yml
+SEED_TEMPLATE=$DIR/1-seed.yml
+CONTAINERS_TEMPLATE=$DIR/2-containers.yml
 
 deploy(){
+  SEED_STACK=$1-seed
 	aws --profile pvitic-administrator \
 	    --region eu-central-1 \
 	    --debug cloudformation deploy \
-      --stack-name $1 \
-      --template $ENV_ADMIN_STORAGE_TEMPLATE \
+      --stack-name $SEED_STACK \
+      --template $SEED_TEMPLATE \
       --capabilities CAPABILITY_NAMED_IAM \
-  && echo "export AWS_ACCESS_KEY_ID=$(aws cloudformation describe-stacks --stack-name $1 | \
+  && echo "export AWS_ACCESS_KEY_ID=$(aws cloudformation describe-stacks --stack-name $SEED_STACK | \
       jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "AccessKey") | .OutputValue')" >> .env \
-  && echo "export AWS_SECRET_ACCESS_KEY=$(aws cloudformation describe-stacks --stack-name $1 | \
+  && echo "export AWS_SECRET_ACCESS_KEY=$(aws cloudformation describe-stacks --stack-name $SEED_STACK | \
       jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "SecretKey") | .OutputValue')" >> .env \
-  && echo "export BUCKET_NAME=$(aws cloudformation describe-stacks --stack-name $1 | \
+  && echo "export BUCKET_NAME=$(aws cloudformation describe-stacks --stack-name $SEED_STACK | \
     jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "BucketName") | .OutputValue')" >> .env \
   && source .env \
   && aws sts get-caller-identity \
   && aws --debug s3 cp . s3://$BUCKET_NAME/ --recursive --exclude "*" --include "*.yml"
+
+  CONTAINERS_STACK=$1-containers
+  aws --debug cloudformation deploy \
+      --stack-name $CONTAINERS_STACK \
+      --template $CONTAINERS_TEMPLATE \
+      --parameter-overrides ImageRepositoryName=mongodb
+
   rm .env
 }
 
 delete(){
+  # TODO delete containers then delete seed
 	aws --profile pvitic-administrator \
 	    --region eu-central-1 \
 	    --debug cloudformation delete-stack \
@@ -30,22 +41,29 @@ delete(){
 }
 
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 1 ]; then
   echo 1>&2 "Usage: $0 environment action"
   exit 3
 fi
 
-ENVIRONMENT_NAME=$1
-ACTION=$2
+ACTION=$1
 
 case $ACTION in
 deploy)
+  ENVIRONMENT_NAME=$(cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1)$ENVIRONMENT_NAME_SUFFIX
   echo "Deploying ${ENVIRONMENT_NAME} environment"
   deploy "$ENVIRONMENT_NAME"
   ;;
 delete)
-  echo "Deleting ${ENVIRONMENT_NAME} environment"
-  delete "$ENVIRONMENT_NAME"
+  if [ -z "$2" ]
+  then
+      echo ERROR: "delete requires environment id"
+      exit 1
+  else
+      ENVIRONMENT_NAME=$2$ENVIRONMENT_NAME_SUFFIX
+      echo "Deleting ${ENVIRONMENT_NAME} environment"
+      delete "$ENVIRONMENT_NAME"
+  fi
   ;;
 *)
     echo "-- Action Options --"
